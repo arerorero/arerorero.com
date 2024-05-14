3e5f17fda11feef85b61e9448be1774ba28d6799
Filippo Grassano <com.filippo@outlook.com>
2024-05-14 15:23:23 -0300

    route for projects and about

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import NotFound from "../views/NotFound.vue";
+import Projects from "../views/portifolio/Projects.vue";
+import Arerorero from "../views/portifolio/Arerorero.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: Home,
+    children: [
+      {
+        path: "/projects",
+        name: "projects",
+        component: Projects,
+      },
+      {
+        path: "/",
+        name: "about-arerorero",
+        component: Arerorero,
+      },
+    ],
   },
   // error 404
   {
