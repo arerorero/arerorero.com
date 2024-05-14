e372b71f70ebedda278832b227695e9800b03278
Filippo Grassano <com.filippo@outlook.com>
2024-05-14 15:23:08 -0300

    about and projects created

---
+++ src/views/portifolio/Arerorero.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>about arerorero</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "arerorero",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #fff;
+  font-size: 10vh;
+  text-align: center;
+}
+</style>
---
+++ src/views/portifolio/Projects.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Projects</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Projects",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #fff;
+  font-size: 20vh;
+  text-align: center;
+}
+</style>
